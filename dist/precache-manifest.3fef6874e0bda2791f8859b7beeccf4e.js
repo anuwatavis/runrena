cb545c4193d8dcbc6dfdf63406d5e08e14711622
anuwatavis <anuwat.avis@gmail.com>
2020-03-29 18:53:24 +0700

    deploy to filebase

---
+++ dist/precache-manifest.3fef6874e0bda2791f8859b7beeccf4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affa43161f64eb43e427eec82bc912b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1151a4a60a532a1b687",
+    "url": "/static/css/2.3c81c441.chunk.css"
+  },
+  {
+    "revision": "30ea68cfeb4bfea631fc",
+    "url": "/static/css/main.00057a11.chunk.css"
+  },
+  {
+    "revision": "e1151a4a60a532a1b687",
+    "url": "/static/js/2.162e2fe2.chunk.js"
+  },
+  {
+    "revision": "4d68cb3ca51ee47dce695bbf94cb9930",
+    "url": "/static/js/2.162e2fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ea68cfeb4bfea631fc",
+    "url": "/static/js/main.f05dd6a1.chunk.js"
+  },
+  {
+    "revision": "816d1783253290518894",
+    "url": "/static/js/runtime-main.a223b992.js"
+  }
+]);
